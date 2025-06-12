b40ea4bfd08c1bd14475b3a271d3fb6d0459e21d
Nishant Verma <vnishant148@gmail.com>
2025-06-12 05:41:17 -0600

    Initial commit

---
+++ Source/GPPatterns/GPPatterns.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GPPatterns.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GPPatterns, "GPPatterns" );
