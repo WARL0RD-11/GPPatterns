81bff46d1a3d8bf9be8a0964ab1b0d667ef1c43d
Nishant Verma <vnishant148@gmail.com>
2025-06-13 04:26:25 -0600

    Adding Boilerplate steps

--- Source/GPPatterns/Characters/MyPlayerController.cpp
+++ Source/GPPatterns/Characters/MyPlayerController.cpp
@@ -2,4 +2,23 @@
 
 
 #include "MyPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "MyCharacter.h"
 
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+}
+
+void AMyPlayerController::OnJumpAction(const FInputActionValue& ActionValue)
+{
+
+}
--- Source/GPPatterns/Characters/MyPlayerController.h
+++ Source/GPPatterns/Characters/MyPlayerController.h
@@ -4,14 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputAction.h"
 #include "MyPlayerController.generated.h"
 
 /**
  * 
  */
+
+class UInputAction;
+class ICommand;
+class UUserWidget;
+
+
+UENUM()
+enum class EInputAction : uint8
+{
+	None,
+	Jump,
+};
+
+
 UCLASS()
 class GPPATTERNS_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* PlayerMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* JumpAction;
+
+private:
+	TMap<EInputAction, TSharedPtr<ICommand>> CommandMap;
+
+	void OnJumpAction(const FInputActionValue& ActionValue);
 };
